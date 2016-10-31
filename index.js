db09da037b587b199b553aa555d4d3872643afd8
Juin Chiu <davidjuin0519@gmail.com>
2016-11-01 00:56:27 +0800

    Add github api callback

--- index.js
+++ index.js
@@ -1,8 +1,16 @@
 var express = require('express')
+var bodyParser = require('body-parser')
+
 var app = express()
+app.use(bodyParser.json());
 
 app.get('/', function (req, res) {
   res.send('Hello World!')
+})
+
+app.post('/github_api_callback', function (req, res) {
+  console.log(req.body.action)
+  res.send('callboak!')
 })
 
 app.listen(3000, function () {
